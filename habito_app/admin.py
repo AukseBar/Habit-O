7db3f1c3d7a32a1b75e84f2cc2d40d5916f9470b
Luca Bisin <2289370b@student.gla.ac.uk>
2017-03-07 12:35:39 +0000

    Added Habit model (no achievements)

--- habito_app/admin.py
+++ habito_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from habito_app.models import Habit
 
-# Register your models here.
+# Pre-populate habit slug in admin page
+class HabitAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('title',)}
+	
+admin.site.register(Habit, HabitAdmin)
--- habito_app/models.py
+++ habito_app/models.py
@@ -1,5 +1,51 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from datetime import date
+import json
 
-# Create your models here.
+# Habit model
+class Habit(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=128)
+	description = models.TextField()
+	# setting auto_now_add will cause the created field to be not editable
+	# unsure if this should be the case...
+	created = models.DateField(default=date.today)
+	days = models.TextField(max_length=128, default={})
+	slug = models.SlugField()
+	
+	# Set as unique the combination title + user (a user cannot create two habits with the same title)
+	class Meta:
+		unique_together = ('user', 'title')
+		
+	# Returns days as a dictionary.
+	# A dictionary cannot be directly stored in the database, 
+	# so the value of days field must be parsed to a json object in order to be used
+	def getDaysList(self):
+		return json.loads(self.days)
+	
+	# Checks days starting from creation date until now
+	# and sets empty days to 0
+	def checkDays(self):
+		days = json.loads(self.days)
+		start_date = self.created
+		now_date = date.today()
+		diff_days = (now_date - start_date).days
+		for d in range(1, diff_days + 1):
+			if str(d) not in days:
+				days[str(d)] = 0
+		self.days = json.dumps(days)
+		self.save()
+	
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Habit, self).save(*args, **kwargs)
+		
+	def __str__(self):
+		return self.title
+	
+	def __unicode__(self):
+		return self.title
+
