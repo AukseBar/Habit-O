860a4a42943117de201a24849cb3947318f07c52
Adele Trijonyte <Adele Trijonyte>
2017-03-14 16:08:11 +0000

    changed name of template dir of habito_app

--- habito_app/urls.py
+++ habito_app/urls.py
@@ -4,6 +4,8 @@ from habito_app import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
+    # url(r'^habit/', views.about, name='habit'),
+
     # I removed this and put it in the habito directory
     # in the book it said to not put it in the app directory
     # url(r'^accounts/', include('registration.backends.simple.urls')),
