3507ef4ed48043cb51edecdf6f998b161c815e07
Aravind Kumar <Aravind Kumar>
2017-03-17 00:51:45 +0000

    fixing issues

--- habito_app/views.py
+++ habito_app/views.py
@@ -25,14 +25,60 @@ def show_user(request):
     response = render(request, 'habito_app/test_user.html', context=context_dict)
     return response
 
-
 def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        user_form = UserForm(data=request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+        
     response = render(request, 'habito_app/register.html')
     return response
 
-def login(request):
-    response = render(request, 'habito_app/login.html')
-    return response
+def user_login(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+
+            if user.is_active:
+
+                login(request, user)
+                return HttpResponseRedirect("/habito_app/")
+            else:
+                return HttpResponse("account diabled")
+        else:
+            return HttpResponse("invalid login details")
+    else:
+        return render(request, 'habito_app/login.html',{})
+
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("FUck OFF")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 # Shows details of a single habit
 def show_habit(request, habit_title_slug):
